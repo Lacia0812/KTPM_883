a59cb421f38e6992fa60776132b782961f46c725
Lacia <son345789@gmail.com>
2025-03-05 19:05:34 +0700

    Lab2

--- ASC.WEB/Data/ApplicationDbContext.cs
+++ ASC.WEB/Data/ApplicationDbContext.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ASC.Model.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace ASC.WEB.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public virtual DbSet<MasterDataKey> MasterDataKeys { get; set; }
+        public virtual DbSet<MasterDataValue> MasterDataValues { get; set; }
+        public virtual DbSet<ServiceRequest> ServiceRequests { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+            Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<MasterDataKey>()
+                .HasKey(c => new { c.PartitionKey, c.RowKey });
+            builder.Entity<MasterDataValue>()
+                .HasKey(c => new { c.PartitionKey, c.RowKey });
+            builder.Entity<ServiceRequest>()
+                .HasKey(c => new { c.PartitionKey, c.RowKey });
+
+            base.OnModelCreating(builder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            // Bỏ qua cảnh báo về các thay đổi mô hình chưa được di chuyển
+            optionsBuilder.ConfigureWarnings(warnings =>
+                warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
         }
     }
 }
--- ASC.WEB/Program.cs
+++ ASC.WEB/Program.cs
@@ -1,13 +1,27 @@
+using ASC.DataAccess.Interfaces;
+using ASC.DataAccess;
 using ASC.WEB.Configuration;
 using ASC.WEB.Data;
+using ASC.WEB.Data.Interfaces;
 using ASC.WEB.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>((options) =>
+{
+    options.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddScoped<DbContext, ApplicationDbContext>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -22,6 +36,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddTransient<IEmailSender, AuthMessgageSender>();
 builder.Services.AddTransient<ISmsSender, AuthMessgageSender>();
+builder.Services.AddSingleton<IIdentitySeed, IdentitySeed>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
@@ -48,5 +64,14 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var storageSeed = scope.ServiceProvider.GetRequiredService<IIdentitySeed>();
+    await storageSeed.Seed(
+        scope.ServiceProvider.GetService<UserManager<IdentityUser>>(),
+        scope.ServiceProvider.GetService<RoleManager<IdentityRole>>(),
+        scope.ServiceProvider.GetService<IOptions<ApplicationSettings>>());
+}
 
 app.Run();
